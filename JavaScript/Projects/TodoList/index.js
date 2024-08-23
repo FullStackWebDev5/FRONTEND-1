dfee46134610d8c5f2885f570452083812942a99
AnkitKumarMishra5 <ankit5kumar14@gmail.com>
2024-08-23 23:55:34 +0530

    JavaScript: Task Manager App: Improvised

--- JavaScript/Projects/TodoList/index.js
+++ JavaScript/Projects/TodoList/index.js
@@ -11,12 +11,43 @@ newTaskInput.addEventListener('input', (event) => {
 })
 
 function addTask() {
-  const newItem = document.getElementById('new-task').value
+  const todo = document.getElementById('new-task').value
 
   const shoppingList = document.getElementById('shopping-list')
   const li = document.createElement('li')
-  li.innerText = newItem
+
+  const leftPart = document.createElement('span')
+  leftPart.innerText = todo
+
+  const rightPart = document.createElement('span')
+
+  const markAsCompletedBtn = document.createElement('button')
+  markAsCompletedBtn.innerText = '✅'
+  markAsCompletedBtn.classList.add('mark-btn')
+
+  const deleteButton = document.createElement('button')
+  deleteButton.innerText = '❌'
+  deleteButton.classList.add('delete-btn')
+
+  markAsCompletedBtn.addEventListener('click', () => {
+    leftPart.classList.add('completed')
+    markAsCompletedBtn.style.display = 'none'
+  })
+
+  deleteButton.addEventListener('click', () => {
+    li.remove()
+  })
+
+  rightPart.append(markAsCompletedBtn, editButton, deleteButton)
+
+  li.append(leftPart, rightPart)
   shoppingList.append(li)
 
   document.getElementById('new-task').value = ''
 }
+
+/*
+  Editing:
+    - Delete the old todo
+    - Create a new todo with new data
+*/
